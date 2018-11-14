12d1ee98971680c36392d9c356db74d4ab20af44
kouzouiiimaizumi <imaizumi@nacalai.co.jp>
2018-11-14 23:15:47 +0000

    bootstrap design

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.all
+    @messages = Message.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def show
